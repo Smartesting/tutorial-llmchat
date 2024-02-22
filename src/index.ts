3a865a4adc66b295101fe03e9578b3db9bc2b75a
alevern <alexandre.vernotte@smartesting.com>
2024-02-22 18:31:15 +0100

    add types and llm provider clients

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,27 @@
 import { Elysia } from "elysia";
+import OpenAI from 'openai'
+import MistralClient from '@mistralai/mistralai'
+
+type Message = {
+  role: "system" | "user" | "assistant";
+  message: string;
+};
+type Conversation = Message[];
+
+const openai = new OpenAI({
+    apiKey: Bun.env.OPENAI_API_KEY
+})
+const mistral = new MistralClient(Bun.env.MISTRALAI_API_KEY)
+const systemMessage: Message = {
+    role: 'system',
+    message: "You are a helpful assistant, provide answsers to the user's questions to the best of your ability. " +
+        "Refrein from inventing knowledge, people are counting on you. "
+}
 
 const indexPlugin = new Elysia()
-    .get('/', () => {
+    .state('conversation', [systemMessage] as Conversation)
+    .get('/', ({store}) => {
+        store.conversation = [systemMessage] as Conversation;
        return Bun.file('views/index.html');
     })
 
