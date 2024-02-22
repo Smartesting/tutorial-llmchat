0b29860a83e5060e04e2d8a8352fe8322abc9211
alevern <alexandre.vernotte@smartesting.com>
2024-02-22 17:35:06 +0100

    create index route

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const indexPlugin = new Elysia()
+    .get('/', () => {
+       return Bun.file('views/index.html');
+    })
+
+const app = new Elysia()
+  .use(indexPlugin)
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
