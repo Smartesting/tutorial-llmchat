06e333c3cc225a533a9c9266acfc9cd36922ab8e
alevern <alexandre.vernotte@smartesting.com>
2024-02-23 15:56:35 +0100

    add /chat route

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
 import OpenAI from 'openai'
 import MistralClient from '@mistralai/mistralai'
 import Stream from "@elysiajs/stream";
 
 type Message = {
     role: "system" | "user" | "assistant";
-    message: string;
+    content: string;
 };
 type Conversation = Message[];
 
@@ -15,8 +15,8 @@ const openai = new OpenAI({
 const mistral = new MistralClient(Bun.env.MISTRALAI_API_KEY)
 const systemMessage: Message = {
     role: 'system',
-    message: "You are a helpful assistant, provide answsers to the user's questions to the best of your ability. " +
-        "Refrein from inventing knowledge, people are counting on you. "
+    content: "You are a helpful assistant, provide answers to the user's questions to the best of your ability. " +
+        "Refrain from inventing knowledge, people are counting on you. "
 }
 
 const indexPlugin = new Elysia()
@@ -26,7 +26,6 @@ const indexPlugin = new Elysia()
         return Bun.file('views/index.html');
     })
 
-
 const streamPlugin = new Elysia()
     .state('stream', new Stream<string>())
     .get('/chat/stream', ({ store }) => {
@@ -34,9 +33,52 @@ const streamPlugin = new Elysia()
         return store.stream
     })
 
-const app = new Elysia()
+const chatPlugin = new Elysia()
     .use(indexPlugin)
     .use(streamPlugin)
+    .derive(async () => {
+        const userMessageId = Date.now().toString()
+        return { userMessageId };
+    })
+    .onResponse(({ store, userMessageId }) => {
+        streamOpenAIResponse(store.stream, store.conversation, userMessageId).then(
+            (llmMessage) => store.conversation.push(llmMessage)
+        )
+    })
+    .post('/chat', async ({ userMessageId, store, set, body }) => {
+        try {
+            const userMessage: Message = { role: 'user', content: body.usermessage }
+            store.conversation.push(userMessage)
+            return `<div>${body.usermessage}</div>
+            <div id="${userMessageId}"></div>`
+        } catch (error) {
+            set['status'] = 500
+            return 'An error occurred while processing your request.'
+        }
+    }, {
+        body: t.Object({
+            usermessage: t.String()
+        })
+    })
+
+async function streamOpenAIResponse(sseStream: Stream<string>, conv: Conversation, assistantMessageId: string): Promise<Message> {
+    const openaiStream = await openai.chat.completions.create({
+        model: 'gpt-3.5-turbo',
+        messages: conv,
+        stream: true,
+    })
+    let fullMessage = ''
+    for await (const chunk of openaiStream) {
+        const delta = chunk.choices[0]?.delta?.content || ''
+        fullMessage += delta
+        sseStream.event = assistantMessageId
+        sseStream.send(fullMessage.replace(/(\r\n|\n|\r)/gm, "&#10;"))
+    }
+    return { role: 'assistant', content: fullMessage }
+}
+
+const app = new Elysia()
+    .use(chatPlugin)
     .listen(3000);
 
 console.log(
